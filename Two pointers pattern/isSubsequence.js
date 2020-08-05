e3333b4945d40513904ce343a6ed579f76c3589f
gastonbordet <gastonbordet95@gmail.com>
2020-08-05 20:46:47 -0300

    Se agrego ejercicio para two pointers pattern

---
+++ Two pointers pattern/isSubsequence.js
@@ -0,0 +1,17 @@
+const isSubsequence = (str1, str2) => {
+  var i = 0;
+  var j = 0;
+  if (!str1) return true;
+  while (j < str2.length) {
+    if (str2[j] === str1[i]) i++;
+    if (i === str1.length) return true;
+    j++;
+  }
+  return false;
+};
+
+console.log(isSubsequence("hello", "hello world")); // true;
+console.log(isSubsequence("sing", "sting")); // true
+console.log(isSubsequence("sing", "strign")); // false
+console.log(isSubsequence("abc", "abracadabra")); // true
+console.log(isSubsequence("abc", "acb")); // false
