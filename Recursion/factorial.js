3c99e53131834ce94c50f82556e347d60fcc4cb8
gastonbordet <gastonbordet95@gmail.com>
2020-08-10 23:05:22 -0300

    Se cambia sintaxis ejercicios recursion

--- Recursion/factorial.js
+++ Recursion/factorial.js
@@ -1,8 +1,4 @@
-function factorial(num) {
-  if (num === 0) return 1;
-
-  return num * factorial(num - 1);
-}
+const factorial = (num) => (num === 0 ? 1 : num * factorial(num - 1));
 
 console.log(factorial(1)); // 1
 console.log(factorial(2)); // 2
--- Recursion/power.js
+++ Recursion/power.js
@@ -1,7 +1,5 @@
-function power(base, exponent) {
-  if (exponent === 0) return 1;
-  return base * power(base, exponent - 1);
-}
+const power = (base, exponent) =>
+  exponent === 0 ? 1 : base * power(base, exponent - 1);
 
 console.log(power(2, 0)); // 1
 console.log(power(2, 3)); // 8
