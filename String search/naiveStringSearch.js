811defe92af2899e93cb2b719a03c19cab736441
gastonbordet <gastonbordet95@gmail.com>
2020-09-21 19:15:16 -0300

    Se agrega ImprovedbasicStringSearch

--- String search/naiveStringSearch.js
+++ String search/naiveStringSearch.js
@@ -20,8 +20,22 @@ function basicStringSearch(str, word) {
   return matches;
 }
 
+function ImprovedbasicStringSearch(str, word) {
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < word.length; j++) {
+      if (word[j] !== str[i + j]) break;
+
+      if (j === word.length - 1) count++;
+    }
+  }
+
+  return count;
+}
+
 // Should return count of matches
-console.log(basicStringSearch("wowomgzomg", "omg")); // 2
-console.log(basicStringSearch("wogwomegomgzomg", "omg")); // 2
-console.log(basicStringSearch("lorie lolredlolied lolrd", "lolr")); // 2
-console.log(basicStringSearch("lorie loled", "lolr")); // 0
+console.log(ImprovedbasicStringSearch("womwoomgzomg", "omg")); // 2
+console.log(ImprovedbasicStringSearch("wogwomegomgzomg", "omg")); // 2
+console.log(ImprovedbasicStringSearch("lorie lolredlolied lolrd", "lolr")); // 2
+console.log(ImprovedbasicStringSearch("lorie loled", "lolr")); // 0
