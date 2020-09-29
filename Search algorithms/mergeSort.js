dff4aa2718107011d027c3fa24acf63cbc458b60
gastonbordet <gastonbordet95@gmail.com>
2020-09-29 19:01:07 -0300

    Se termina mergeSort

--- Search algorithms/mergeSort.js
+++ Search algorithms/mergeSort.js
@@ -25,7 +25,19 @@ function merge(arr1, arr2) {
 
   return result;
 }
-console.log(merge([1, 5], [3])); // 1,3,5
-console.log(merge([1, 4, 5], [2, 3, 6, 7, 9])); // 1,2,3,4,5,6,7,9
-console.log(merge([2, 3, 6, 7, 9], [1, 4, 5])); // 1,2,3,4,5,6,7,9
-console.log(merge([2, 3, 6, 7, 7, 9], [1, 4, 5])); // 1,2,3,4,5,6,7,7,9
+
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+
+  let right = mergeSort(arr.slice(0, Math.floor(arr.length / 2)));
+  let left = mergeSort(arr.slice(Math.floor(arr.length / 2)));
+
+  return merge(right, left);
+}
+
+// console.log(merge([1, 5], [3])); // 1,3,5
+// console.log(merge([1, 4, 5], [2, 3, 6, 7, 9])); // 1,2,3,4,5,6,7,9
+// console.log(merge([2, 3, 6, 7, 9], [1, 4, 5])); // 1,2,3,4,5,6,7,9
+// console.log(merge([2, 3, 6, 7, 7, 9], [1, 4, 5])); // 1,2,3,4,5,6,7,7,9
+
+console.log(mergeSort([10, 24, 76, 73])); // 10, 24, 73, 76
